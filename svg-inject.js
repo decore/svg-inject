e402f908bace34d3935322b68c637a5ef2fb3d4e
Eugen Soloviov <suenot@gmail.com>
2015-10-03 18:15:08 +0400

    ajax svg

--- svg-inject.js
+++ svg-inject.js
@@ -1,30 +1,14 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-// 	var elements = document.querySelectorAll('.isvg');
-// 	for (var i = 0, max = elements.length; i < max; i++) {
-// 		var request = new XMLHttpRequest();
-// 		request.open('GET', '/img/svg/sketch-front.svg', true);
-// 		request.onload = function() {
-// 			if (request.status >= 200 && request.status < 400) {
-// 				var data = JSON.parse(request.responseText);
-// 				console.log('test');
-// 				console.log(data);
-// 			} else {
-// 			}
-// 		};
-// 		request.send();
-// 	};
-// });
 document.addEventListener("DOMContentLoaded", function(event) {
-	// var icons = document.getElementsByClassName('icon');
-	// for (var i=0; i<=icons.length; i++) {
-		// var icon = icons[i];
-		// console.log(icon);
+	var icons = document.getElementsByClassName('isvg');
+	console.log(icons);
+	for (var i=0; i<icons.length; i++) {
+		var icon = icons[i];
+		console.log(icon);
 		var http = new XMLHttpRequest();
 		http.open('GET', '/img/svg/sketch-front.svg', true);
 		http.onload = function () {
-			// icon.insertAdjacentHTML('afterbegin', http.responseText);
-			console.log(http.responseText);
+			icon.innerHTML = http.responseText;
 		}
 		http.send();
-	// };
+	};
 });
