f96866035f361a110efddfe38c7c347f8343c380
Eugen Soloviov <suenot@gmail.com>
2015-10-03 23:42:58 +0400

    finish

--- svg-inject.js
+++ svg-inject.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var icons = document.getElementsByClassName('isvg');
-	console.log(icons);
-	for (var i=0; i<icons.length; i++) {
-		var icon = icons[i];
-		console.log(icon);
+	// console.log(icons);
+	// console.log(typeof(icons));
+	// icons.forEach(function(item, i, icons) {
+	Array.prototype.forEach.call(icons, function(icon, i){
+		// var icon = item;
+		// console.log(icon);
 		var http = new XMLHttpRequest();
-		http.open('GET', '/img/svg/sketch-front.svg', true);
+		http.open('GET', '/img/svg/' + icon.getAttribute('data-icon') + '.svg', true);
 		http.onload = function () {
+			// icon.outerHTML = '123456789';
 			icon.innerHTML = http.responseText;
 		}
 		http.send();
-	};
+	});
 });
+// GMne0eKs0e0B
+// 909PyEz0PX0x
