086bfdb6a97bd1e7d562b79b78bda222173bc4d1
Eugen Soloviov <suenot@gmail.com>
2015-10-03 18:02:38 +0400

    ajax

--- svg-inject.js
+++ svg-inject.js
@@ -1,14 +1,30 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+// 	var elements = document.querySelectorAll('.isvg');
+// 	for (var i = 0, max = elements.length; i < max; i++) {
+// 		var request = new XMLHttpRequest();
+// 		request.open('GET', '/img/svg/sketch-front.svg', true);
+// 		request.onload = function() {
+// 			if (request.status >= 200 && request.status < 400) {
+// 				var data = JSON.parse(request.responseText);
+// 				console.log('test');
+// 				console.log(data);
+// 			} else {
+// 			}
+// 		};
+// 		request.send();
+// 	};
+// });
 document.addEventListener("DOMContentLoaded", function(event) {
-	var elements = document.querySelectorAll('.isvg');
-	for (var i = 0, max = elements.length; i < max; i++) {
-		var request = new XMLHttpRequest();
-		request.open('GET', 'http://prestapro-test.verstalishe.ru/img/svg/' + elements[i].getAttribute('data-icon') + '.svg', true);
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 400) {
-				var data = JSON.parse(request.responseText);
-				 console.log(data);
-			} else {
-			}
-		};
-	};
+	// var icons = document.getElementsByClassName('icon');
+	// for (var i=0; i<=icons.length; i++) {
+		// var icon = icons[i];
+		// console.log(icon);
+		var http = new XMLHttpRequest();
+		http.open('GET', '/img/svg/sketch-front.svg', true);
+		http.onload = function () {
+			// icon.insertAdjacentHTML('afterbegin', http.responseText);
+			console.log(http.responseText);
+		}
+		http.send();
+	// };
 });
